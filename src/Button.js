04192b4860ff9638be516ccbbc8aaeac25323573
TomyRavn <godqwer123@naver.com>
2021-11-23 15:37:59 +0900

    create-react-app(module)

---
+++ src/Button.js
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+function Button({ text }) {
+    // <button style={{ backgroundColor: "tomato", color: "white" }}>
+    //   {text}
+    // </button>
+  return (
+    <button className={styles.btn}>
+        {text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Button;
