44b455805df0b5df208cff67143ddeaf0fa79435
TomyRavn <godqwer123@naver.com>
2021-11-24 11:06:28 +0900

    Clean up Code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,61 @@
-import Button from "./Button.js";
+// import Button from "./Button.js";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
-  const onlyOnce = () => {
-    console.log("I run only once.");
-  };
-  useEffect(onlyOnce, []);
+function Hello() {
+  //1. 개별 function 작성
+  // const destroyedFn = () => {
+  //   console.log("destroyed :<");
+  // };
+  // const effectFn = () => {
+  //   console.log("created :)");
+  //   return destroyedFn;
+  // };
+  // useEffect(
+  //   effectFn,
+  //   //return () => console.log("destroyed :<"); //Clean up Function
+  //   []
+  // );
+
+  //2. useEffect 안에 통합(more used)
   useEffect(() => {
-    if (keyword !== "") console.log("SEARCH FOR", keyword);
-  }, [keyword]);
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  // const [counter, setValue] = useState(0);
+  // const [keyword, setKeyword] = useState("");
+  // const onClick = () => setValue((prev) => prev + 1);
+  // const onChange = (event) => setKeyword(event.target.value);
+  // console.log("I run all the time");
+  // const onlyOnce = () => {
+  //   console.log("I run only once.");
+  // };
+  // useEffect(onlyOnce, []);
+  // useEffect(() => {
+  //   if (keyword !== "") console.log("SEARCH FOR", keyword);
+  // }, [keyword]);
+  // return (
+  //   <div>
+  //     <input
+  //       value={keyword}
+  //       onChange={onChange}
+  //       type="text"
+  //       placeholder="Search here."
+  //     />
+  //     <h1>{counter}</h1>
+  //     <Button text="click me" onClick={onClick} />
+  //   </div>
+  // );
+
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here."
-      />
-      <h1>{counter}</h1>
-      <Button text="click me" onClick={onClick} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
