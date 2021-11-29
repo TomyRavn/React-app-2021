343375d38f2f93619a91acdaaab10ec5431ee7ba
TomyRavn <godqwer123@naver.com>
2021-11-29 13:07:57 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 //1. To Do List
 // function App() {
@@ -77,53 +78,17 @@ import { useEffect } from "react/cjs/react.development";
 
 //3. Movie app
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rate=9.9&sort_by=year`
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  // useEffect(() => {
-  //   fetch(
-  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.0&sort_by=year`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setMovies(json.data.movies);
-  //       setLoading(false);
-  //     });
-  // }, []);
-  console.log(movies);
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((genre) => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
